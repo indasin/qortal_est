968de2f35ebda7aa85c97bdf9876d815784d71f1
indasin <140671521+indasin@users.noreply.github.com>
2023-07-27 14:21:56 +0300

    Add files via upload

---
+++ scripts/add-debian-apt-repo.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Make necessary config and add Qortal Softwares apt repo
+
+# SCript to run UI without sandbox
+echo \'/opt/${productFilename}/qortal-ui\' --no-sandbox > '/opt/${productFilename}/run-ui'
+chmod +x '/opt/${productFilename}/run-ui'
+
+# Link to run-ui
+ln -sf '/opt/${productFilename}/run-ui' '/usr/bin/${executable}'
+
+# SUID chrome-sandbox for Electron 5+
+sudo chown root '/opt/${productFilename}/chrome-sandbox' || true
+sudo chmod 4755 '/opt/${productFilename}/chrome-sandbox' || true
+
+update-mime-database /usr/share/mime || true
+update-desktop-database /usr/share/applications || true
+
+# Install curl if not installed on the system
+if ! which curl; then sudo apt-get --yes install curl; fi
+
+# Install apt repository source list if it does not exist
+if ! grep ^ /etc/apt/sources.list /etc/apt/sources.list.d/* | grep qortal.list; then
+  curl -sS https://update.qortal.online/qortal.gpg | sudo apt-key add -
+  sudo rm -rf /usr/share/keyrings/qortal.gpg
+  sudo apt-key export 856A3FF1 | sudo gpg --dearmour -o /usr/share/keyrings/qortal.gpg
+  sudo rm -rf /etc/apt/sources.list.d/qortal.list
+  echo 'deb [arch=amd64,arm64 signed-by=/usr/share/keyrings/qortal.gpg] https://update.qortal.online/ ./ ' >  /etc/apt/sources.list.d/qortal.list
+fi
---
+++ scripts/notarize.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const { notarize } = require('@electron/notarize');
+
+exports.default = async function notarizing(context) {
+  const { electronPlatformName, appOutDir } = context;  
+  if (electronPlatformName !== 'darwin') {
+    return;
+  }
+
+  const appName = context.packager.appInfo.productFilename;
+
+  return await notarize({
+    appBundleId: 'org.qortal.QortalUI',
+    appPath: `${appOutDir}/${appName}.app`,
+    tool: "notarytool",
+    teamId: process.env.APPLETEAMID,
+    appleId: process.env.APPLEID,
+    appleIdPassword: process.env.APPLEIDPASS,
+  });
+};
